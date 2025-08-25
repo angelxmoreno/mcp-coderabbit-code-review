e6729740a61c53fcd597bee3dd9e74c5b9cdefb6
Angel S. Moreno <angelxmoreno@gmail.com>
2025-08-25 16:04:12 -0400

    feat: updated config to be env aware

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,9 @@
-import type { Config, NODE_ENV } from './types/config.ts';
+import type {Config, NODE_ENV} from './types/config.ts';
+
+const env = (Bun.env.NODE_ENV ?? 'development') as NODE_ENV;
+const isDevelopment = env !== 'production';
 
 export const config: Config = {
-    env: (Bun.env.NODE_ENV ?? 'development') as NODE_ENV,
+    env,
+    isDevelopment,
 };
